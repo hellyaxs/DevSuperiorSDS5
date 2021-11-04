e61b9fc7eb58db615710a283fb0129f6faa016c3
hellyaxs <eliasvitor.dev@gmail.com>
2021-11-04 16:34:46 -0300

    Basic static components

--- frontand/src/App.tsx
+++ frontand/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 
 function App() {
   return (
-    <div className="App">
-     <div className="container">
-       <h1 className="text-primary">hello word</h1>
-     </div>
-    </div>
+    <>
+      <NavBar />
+      <div className="App">
+        
+        <div className="container">
+        <h1 className="text-primary">hello word</h1>
+          <DataTable />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
